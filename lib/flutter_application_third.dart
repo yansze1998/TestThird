6654788967f7bacb02cc6f34d3f99ebcb89f94ff
Choo Yan Sze <Sze@iZyoo-Derek.local>
2022-04-25 17:38:23 +0800

    hehe

--- lib/flutter_application_third.dart
+++ lib/flutter_application_third.dart
@@ -2,10 +2,10 @@ library flutter_application_third;
 
 /// A Calculator.
 class CalculatorThird {
-  /// Returns [value] plus 3.
-  int addThree(int value) => value + 3;
+  /// Returns [value] plus 3 nah.
+  int addThree(int value) => value + 30;
 }
 
 class TestFunc {
-  String getTestString() => "I also donno";
+  String getTestString() => "I also donno ?? updated!!";
 }
